6a1931743b1195e5d3533a760445b4e18e3b9356
carol-palomo <85017737+carol-palomo@users.noreply.github.com>
2021-10-25 18:12:01 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,237 @@
+provider "aws" {
+  region = "us-east-1"
+}
 
+resource "aws_instance" "k8s_proxy" {
+  ami           = "ami-09e67e426f25ce0d7"
+  subnet_id     = "subnet-0ab487dbac2dcfa24"
+  instance_type = "t2.micro"
+  key_name      = "id_rsa_jenkins"
+  associate_public_ip_address = true
+  
+  root_block_device {
+    volume_size           = "8"
+    volume_type           = "gp2"
+    encrypted             = true
+    kms_key_id            = "f48a0432-3f72-4888-9b31-8bdf1c121a4c"
+    delete_on_termination = true
+  }
+  
+  tags = {
+    Name = "k8s-haproxy"
+  }
+  vpc_security_group_ids = [aws_security_group.acessos.id]
+}
+
+resource "aws_instance" "k8s_masters" {
+  ami           = "ami-09e67e426f25ce0d7"
+  subnet_id     = "subnet-0ab487dbac2dcfa24"
+  instance_type = "t2.large"
+  key_name      = "id_rsa_jenkins"
+  count         = 3
+  associate_public_ip_address = true
+  
+  root_block_device {
+    volume_size           = "8"
+    volume_type           = "gp2"
+    encrypted             = true
+    kms_key_id            = "f48a0432-3f72-4888-9b31-8bdf1c121a4c"
+    delete_on_termination = true  
+  }
+  
+  tags = {
+    Name = "k8s-master-${count.index}"
+  }
+  vpc_security_group_ids = [aws_security_group.acessos_master.id]
+  depends_on = [
+    aws_instance.k8s_workers,
+  ]
+}
+
+resource "aws_instance" "k8s_workers" {
+  ami           = "ami-09e67e426f25ce0d7"
+  subnet_id     = "subnet-0ab487dbac2dcfa24"
+  instance_type = "t2.medium"
+  key_name      = "id_rsa_jenkins"
+  count         = 3
+  associate_public_ip_address = true
+  
+  root_block_device {
+    volume_size           = "8"
+    volume_type           = "gp2"
+    encrypted             = true
+    kms_key_id            = "f48a0432-3f72-4888-9b31-8bdf1c121a4c"
+    delete_on_termination = true
+  }  
+  
+  tags = {
+    Name = "k8s_workers-${count.index}"
+  }
+  vpc_security_group_ids = [aws_security_group.acessos.id]
+}
+
+
+resource "aws_security_group" "acessos_master" {
+  name        = "k8s-acessos_master"
+  description = "acessos inbound traffic"
+
+  ingress = [
+    {
+      description      = "SSH from VPC"
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids = null,
+      security_groups: null,
+      self: null
+    },
+    {
+      cidr_blocks      = []
+      description      = "Libera acesso k8s_masters"
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = true
+      to_port          = 0
+    },
+    {
+      cidr_blocks      = []
+      description      = "Libera acesso k8s_workers"
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = [
+        "sg-082aca1fa06121961",
+        //aws_security_group.acessos_master.id
+      ]
+      self             = false
+      to_port          = 0
+    },
+    {
+      cidr_blocks      = [
+        "0.0.0.0/0",
+      ]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 65535
+    },
+  ]
+
+  egress = [
+    {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = [],
+      prefix_list_ids = null,
+      security_groups: null,
+      self: null,
+      description: "Libera dados da rede interna"
+    }
+  ]
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+
+
+resource "aws_security_group" "acessos" {
+  name        = "k8s-workers"
+  description = "acessos inbound traffic"
+
+  ingress = [
+    {
+      description      = "SSH from VPC"
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids = null,
+      security_groups: null,
+      self: null
+    },
+    {
+      cidr_blocks      = []
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = [
+        aws_security_group.acessos_master.id,
+      ]
+      self             = false
+      to_port          = 0
+    },
+    {
+      cidr_blocks      = []
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = true
+      to_port          = 65535
+    },
+  ]
+
+  egress = [
+    {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = [],
+      prefix_list_ids = null,
+      security_groups: null,
+      self: null,
+      description: "Libera dados da rede interna"
+    }
+  ]
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+
+output "k8s-masters" {
+  value = [
+    for key, item in aws_instance.k8s_masters :
+      "k8s-master ${key+1} - ${item.private_ip} - ssh -i ~/Desktop/devops/treinamentoItau ubuntu@${item.public_dns} -o ServerAliveInterval=60"
+  ]
+}
+
+output "output-k8s_workers" {
+  value = [
+    for key, item in aws_instance.k8s_workers :
+      "k8s-workers ${key+1} - ${item.private_ip} - ssh -i ~/Desktop/devops/treinamentoItau ubuntu@${item.public_dns} -o ServerAliveInterval=60"
+  ]
+}
+
+output "output-k8s_proxy" {
+  value = [
+    "k8s_proxy - ${aws_instance.k8s_proxy.private_ip} - ssh -i ~/Desktop/devops/treinamentoItau ubuntu@${aws_instance.k8s_proxy.public_dns} -o ServerAliveInterval=60"
+  ]
+}
+
+output "security-group-workers-e-haproxy" {
+  value = aws_security_group.acessos.id
+}
+
+
+
+# terraform refresh para mostrar o ssh
