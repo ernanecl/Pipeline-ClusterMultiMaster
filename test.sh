ca22dd03e9943eeff330a9d6d88830b959ab62db
ernane <ernanecl@gmail.com>
2021-10-26 13:55:04 -0300

    change file

--- test.sh
+++ test.sh
@@ -1,24 +1,15 @@
-cd ../ansible/01-k8s-install-masters_e_workers
+cd ./ansible/01-k8s-install-masters_e_workers
 
 ANSIBLE_OUT=$(ansible-playbook -i hosts test.yml -u ubuntu --private-key /var/lib/jenkins/.ssh/id_rsa)
-
 echo $ANSIBLE_OUT
 
-
-
-STATUS_K8S=$(echo $ANSIBLE_OUT | grep -oE "(ready.*?.*?)'")
+STATUS_K8S=$(echo $ANSIBLE_OUT | grep -oE "(control-plane.*?.*?)'")
 
 if [[ -z $STATUS_K8S ]]
   then
-
-  echo "::::: kubernetes nao esta configurado :::::"
-
-exit 1
-
-else
-
-echo "::::: cluester kubernetes multi master configurado :::::"
-
-exit 0
-
+    echo "::::: kubernetes nao esta configurado :::::"
+    exit 1
+  else
+    echo "::::: cluster kubernetes multi master configurado :::::"
+    exit 0
 fi
