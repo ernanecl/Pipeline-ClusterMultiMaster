eeba5ea15617089cfdf4a3c941375511c8a59da0
ernane <ernanecl@gmail.com>
2021-10-26 11:11:54 -0300

    add content

---
+++ test.sh
@@ -0,0 +1,24 @@
+cd ../ansible/01-k8s-install-masters_e_workers
+
+ANSIBLE_OUT=$(ansible-playbook -i hosts teste.yml -u ubuntu --private-key /var/lib/jenkins/.ssh/id_rsa)
+
+echo $ANSIBLE_OUT
+
+
+
+STATUS_K8S=$(echo $ANSIBLE_OUT | grep -oE "(ready.*?.*?)'")
+
+if [[ -z $STATUS_K8S ]]
+  then
+
+  echo "::::: kubernetes nao esta configurado :::::"
+
+exit 1
+
+else
+
+echo "::::: cluester kubernetes multi master configurado :::::"
+
+exit 0
+
+fi
