a921c08a26d8a0e6e17389c0f2d06b3fddb9957e
ernane <ernanecl@gmail.com>
2021-10-26 14:42:10 -0300

    add sleep

--- test.sh
+++ test.sh
@@ -3,6 +3,8 @@ cd ./ansible/01-k8s-install-masters_e_workers
 ANSIBLE_OUT=$(ansible-playbook -i hosts test.yml -u ubuntu --private-key /var/lib/jenkins/.ssh/id_rsa)
 echo $ANSIBLE_OUT
 
+sleep 20
+
 STATUS_K8S=$(echo $ANSIBLE_OUT | grep -oE "(control-plane.*?.*?)'")
 
 if [[ -z $STATUS_K8S ]]
